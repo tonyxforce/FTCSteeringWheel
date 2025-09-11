23f69b1963326ed212658d0232a00b21c443c318
tonyxforce <horich.dominik@gmail.com>
2025-09-12 00:13:21 +0200

    Add debug for gamepads

--- public/index.js
+++ public/index.js
@@ -29,6 +29,24 @@ ws.onclose = ()=>{
 	connectWs();
 }
 
+window.addEventListener("gamepadconnected", (e) => {
+	console.log(
+		"Gamepad connected at index %d: %s. %d buttons, %d axes.",
+		e.gamepad.index,
+		e.gamepad.id,
+		e.gamepad.buttons.length,
+		e.gamepad.axes.length,
+	);
+});
+
+window.addEventListener("gamepaddisconnected", (e) => {
+	console.log(
+		"Gamepad disconnected from index %d: %s",
+		e.gamepad.index,
+		e.gamepad.id,
+	);
+});
+
 ws.onmessage = (e)=>{
 	var data = JSON.parse(e.data);
 	switch(data.type){

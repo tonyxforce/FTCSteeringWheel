abf408f36d0d44d29415207b426d03a7c0a69824
tonyxforce <horich.dominik@gmail.com>
2025-09-12 15:31:08 +0200

    Add experimental gamepad support

--- public/index.js
+++ public/index.js
@@ -1,12 +1,65 @@
-
-
-var ws;
-var ip = "localhost";
-var status = {}
+class ControllerState{
+	constructor(){
+		this.state = {
+			"type": "RECEIVE_GAMEPAD_STATE",
+			"gamepad1": {
+				"left_stick_x": 0,
+				"left_stick_y": 0,
+				"right_stick_x": 0,
+				"right_stick_y": 0,
+				"dpad_up": false,
+				"dpad_down": false,
+				"dpad_left": false,
+				"dpad_right": false,
+				"a": false,
+				"b": false,
+				"x": false,
+				"y": false,
+				"guide": false,
+				"start": false,
+				"back": false,
+				"left_bumper": false,
+				"right_bumper": false,
+				"left_stick_button": false,
+				"right_stick_button": false,
+				"left_trigger": 0,
+				"right_trigger": 0
+			},
+			"gamepad2": {
+				"left_stick_x": 0,
+				"left_stick_y": 0,
+				"right_stick_x": 0,
+				"right_stick_y": 0,
+				"dpad_up": false,
+				"dpad_down": false,
+				"dpad_left": false,
+				"dpad_right": false,
+				"a": false,
+				"b": false,
+				"x": false,
+				"y": false,
+				"guide": false,
+				"start": false,
+				"back": false,
+				"left_bumper": false,
+				"right_bumper": false,
+				"left_stick_button": false,
+				"right_stick_button": false,
+				"left_trigger": 0,
+				"right_trigger": 0
+			}
+		};
+	}
+}
 
 function connectWs() {
-	console.log("Connecting...");
+	wsReconnectTimeout--;
+	if (ws)
+		console.log("Connecting...");
 	ws = new WebSocket(`ws:/${ip}:8000/`);
+	ws.onopen = onopen;
+	ws.onclose = onclose;
+	ws.onmessage = onmessage;
 }
 connectWs();
 
@@ -18,13 +71,34 @@ setInterval(() => {
 	}
 }, 1000)
 
-ws.onopen = ()=>{
+
+function calcWheelSpeed(wheelPos, forward) {
+	// Calculate left and right wheel speeds based on wheel postiion (-1=-90deg, 0=0deg, 1=90deg)
+	// Fully left or right should mean that the wheel stays in one position and the robot turns around it
+	// Wheels should only move forward, never backwards
+	// forward is a value from 0 to 1 that indicates how fast the robot should move forward
+	// returns an object with left and right wheel speeds from 0 to 1
+	// e.g. {left: 0.5, right: 1}
+	return {
+		left: forward * (1 - wheelPos) / 2,
+		right: forward * (1 + wheelPos) / 2
+	};
+};
+
+function onopen() {
 	console.log("open");
 	ws.send(JSON.stringify({ "type": "INIT_OP_MODE", "opModeName": "camtest2" }));
 	ws.send(JSON.stringify({ "type": "START_OP_MODE" }));
+	var controller = new ControllerState();
+	controller.state.controller1.left_stick_x = 1;
+	ws.send(JSON.stringify(controller.state));
+	setTimeout(() => {
+	controller.state.controller1.left_stick_x = 0;
+	ws.send(JSON.stringify(controller.state));
+	}, 1000);
 }
 
-ws.onclose = ()=>{
+function onclose() {
 	console.log("closed");
 	connectWs();
 }
@@ -47,10 +121,10 @@ window.addEventListener("gamepaddisconnected", (e) => {
 	);
 });
 
-ws.onmessage = (e)=>{
+function onmessage(e) {
 	var data = JSON.parse(e.data);
-	switch(data.type){
-		case "RECEIVE_ROBOT_STATUS":{
+	switch (data.type) {
+		case "RECEIVE_ROBOT_STATUS": {
 			status = data.status
 			break;
 		};
@@ -58,8 +132,8 @@ ws.onmessage = (e)=>{
 			var img = document.getElementById("camimg");
 			img.src = "data:image/jpeg;base64," + data.imageString;
 			break;
-			}
-		default:{
+		}
+		default: {
 			console.log(data);
 		};
 
